53b2cb1c6ca8c898b79b4b6c0124da39a1c78cea
shivangiiagarwal1998 <89637465+shivangiiagarwal1998@users.noreply.github.com>
2021-10-07 09:31:59 +0530

    exercise

--- Day2/MainApp.java
+++ Day2/MainApp.java
@@ -3,16 +3,18 @@ package com.day2;
 public class MainApp {
 	
 	public static void main(String[] args) {
+		Account a= new Account();
 		Account a1 = new Current(1001, 2000, "Bl403", "Anu");
 		Account a2= new Savings(2031,50000,"A24C9",5000);
-		tDisplay(Account);
-		public static void tDisplay (Account Account) {
+		tDisplay(a);
+		public static void tDisplay (Account a) {
 			
-			Current c= (Current)Account;
+			Current c= (Current)a;
 			c.display();
-			Savings s= (Savings)Account;
+			Savings s= (Savings)a;
 			s.display();
 		}
 	}
 
 }
+
